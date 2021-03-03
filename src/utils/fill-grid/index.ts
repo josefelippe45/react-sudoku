89c145e1be64e69934744dc4549e51b165d1b9ba
José Felippe <jose.felippe@dsbsolucoes.com.br>
2021-03-03 11:58:35 -0300

    feat: add fill grid function

---
+++ src/utils/fill-grid/index.ts
@@ -0,0 +1,33 @@
+import { GRID, NUMBERS } from 'typings';
+import { isInRow, isInCol, shuffle } from 'utils';
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ * A backtracking/recursive function to check all the possible combinations of numbers until a solution is found
+ * @param grid
+ */
+
+const fillGrid = (grid: GRID) => {
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+    if (grid[row][col] === 0) {
+      shuffle(numbers);
+      for (let value of numbers) {
+        if (!isInRow({ grid, row, value })) {
+          if (!isInCol({ grid, col, value })) {
+            grid[row][col] = value;
+          }
+        }
+      }
+      break;
+    }
+  }
+  grid[row][col] = 0;
+};
+
+export default fillGrid;
