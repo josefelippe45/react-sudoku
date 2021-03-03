242c83fbe83d5f5da95c1a72265906a404ed72e8
José Felippe <jose.felippe@dsbsolucoes.com.br>
2021-03-03 11:57:49 -0300

    add is in column and is in row logic

---
+++ src/utils/is-in/col/index.ts
@@ -0,0 +1,15 @@
+import { GRID, NUMBERS } from 'typings';
+
+interface IInput {
+  grid: GRID;
+  col: number;
+  value: NUMBERS;
+}
+const isInCol = ({ col, grid, value }: IInput): boolean => {
+  for (let i = 0; i < 9; i++) {
+    if (value === grid[i][col]) return true;
+  }
+  return false;
+};
+
+export default isInCol;
