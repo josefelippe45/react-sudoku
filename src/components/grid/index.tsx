7942179a850778c0a95f015b0adb58274206e49d
José Felippe <jose.felippe@dsbsolucoes.com.br>
2021-03-04 10:07:29 -0300

    feat: dispatch createGrid

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,11 +1,17 @@
-import React, { FC, Children } from 'react';
-import { createFullGrid } from 'utils';
+import React, { FC, Children, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createGrid } from 'reducers';
+import { AnyAction, Dispatch } from 'redux';
 import Block from './block';
 import { Container, Row } from './styles';
 
 const Grid: FC = () => {
-  const grid = createFullGrid();
-  console.log(grid);
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+  const create = useCallback(() => dispatch(createGrid()), [dispatch]);
+  useEffect(() => {
+    create();
+  }, [create]);
+
   return (
     <Container>
       {Children.toArray(
