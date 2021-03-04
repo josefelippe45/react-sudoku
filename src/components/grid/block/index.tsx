2233c32ecf8741f447f42e10462084d2efcafd34
José Felippe <jose.felippe@dsbsolucoes.com.br>
2021-03-04 10:14:07 -0300

    feat: mapping data into sudoku grid

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,13 +1,24 @@
 import React, { FC } from 'react';
-
+import { useSelector } from 'react-redux';
+import { IReducer } from 'reducers';
+import { N } from 'typings';
 import { Container } from './styles';
 
 interface BlockProps {
   columnIndex: number;
   rowIndex: number;
 }
-
+interface IState {
+  value: N;
+}
 const Block: FC<BlockProps> = ({ columnIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${columnIndex}`} />;
+  const { value } = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][columnIndex] : 0,
+  }));
+  return (
+    <Container data-cy={`block-${rowIndex}-${columnIndex}`}>
+      {value === 0 ? '' : value}
+    </Container>
+  );
 };
 export default Block;
