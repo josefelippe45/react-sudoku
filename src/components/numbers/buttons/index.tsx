c5d0c4027db11ebca690d7b79b811771830b97f8
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2021-04-30 16:02:53 -0300

    feat: now buttons add numbers to grid

--- src/components/numbers/buttons/index.tsx
+++ src/components/numbers/buttons/index.tsx
@@ -1,13 +1,36 @@
-import React, { FC } from 'react';
-import { NUMBERS } from 'typings';
+import React, { FC, useCallback } from 'react';
+import { NUMBERS, BLOCK_COORDS, N } from 'typings';
 import { Button } from 'components';
+import { useDispatch, useSelector } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
+import { fillBlock, IReducer } from 'reducers';
 
 interface IProps {
   value: NUMBERS;
 }
+interface IState {
+  selectedBlock?: BLOCK_COORDS;
+  selectedValue: N;
+}
 
 const NumberButton: FC<IProps> = ({ value }) => {
-  const fill = () => console.log('fill', value);
+  const { selectedValue, selectedBlock } = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  );
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+  const fill = useCallback(
+    () =>
+      selectedBlock &&
+      selectedValue === 0 &&
+      dispatch(fillBlock(value, selectedBlock)),
+    [dispatch, selectedBlock, selectedValue, value]
+  );
   return <Button onClick={fill}>{value}</Button>;
 };
 
